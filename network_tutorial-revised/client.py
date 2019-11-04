e4174f0d87c0880bbb33e546695f162435086a60
yosia <38096919+yosiarviv@users.noreply.github.com>
2019-11-04 22:38:30 +0200

    at 1:15:03 in the tutorial

--- network_tutorial-revised/client.py
+++ network_tutorial-revised/client.py
@@ -25,7 +25,7 @@ def main():
 
     while run:
         clock.tick(60)
-
+        p2 = n.send(p)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
